9a4638bf2c009a99eb49da7cd10c09d41d228a38
Eder Ruiz <drrzmr@gmail.com>
2018-04-06 04:44:50 -0300

    Improve variable names

--- kafka/data.tf
+++ kafka/data.tf
@@ -1,17 +1,17 @@
 data "azurerm_resource_group" "vnet" {
-  name = "${local.vnet_resource_group_name}"
+  name = "${local.vnet_resource_group}"
 }
 
 data "azurerm_image" "kafka" {
   name_regex          = "${local.image_name}"
-  resource_group_name = "${local.name}"
+  resource_group_name = "${local.image_resource_group}"
   sort_descending     = "1"
 }
 
 data "azurerm_subnet" "kafka" {
   name                 = "${local.name}"
   virtual_network_name = "${local.vnet_name}"
-  resource_group_name  = "${local.vnet_resource_group_name}"
+  resource_group_name  = "${local.vnet_resource_group}"
 }
 
 data "azurerm_resource_group" "kafka" {
