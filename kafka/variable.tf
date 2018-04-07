08969e36afe6cde41beac57d84e6ce91c515472d
Eder Ruiz <drrzmr@gmail.com>
2018-04-07 19:30:49 -0300

    Rename and refactor kafka/zookeeper submodule

--- kafka/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "zookeeper_quorum" {}
-variable "ssh_pub_key_file" {}
-variable "name" {}
-variable "image_name" {}
-variable "image_resource_group" {}
-variable "vnet_resource_group" {}
-variable "vnet_name" {}
